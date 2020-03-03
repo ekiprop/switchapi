f571aef3f21ee0e23fda1c91d8390dc23f8a467f
ekiprop <evanskiprop1@gmail.com>
2020-03-03 21:35:40 +0300

    viewsets

--- switchapi/smartswitchapi/views.py
+++ switchapi/smartswitchapi/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from smartswitchapi.models import Switchapi
+from rest_framework import viewsets
+from . serializers import SwitchapiSerializer
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'switchapi': reverse('switchapi-list', request=request, format=format)
+    })
+
+
+class SwitchapiViewSet(viewsets.ModelViewSet):
+
+    queryset = Switchapi.objects.all()
+    serializer_class = SwitchapiSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
