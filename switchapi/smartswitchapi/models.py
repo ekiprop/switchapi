4a183784ce08877442c115d4bc31511b71da793b
ekiprop <evanskiprop1@gmail.com>
2020-03-03 21:11:01 +0300

    models migrations

--- switchapi/smartswitchapi/models.py
+++ switchapi/smartswitchapi/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class Switchapi(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    switch0 = models.CharField(max_length=50, default=None)
+    switch1 = models.CharField(max_length=50, default=None)
+    switch2 = models.CharField(max_length=50, default=None)
+    switch3 = models.CharField(max_length=50, default=None)
+    phone_no = models.IntegerField()
+
+    def __str__(self):
+        return self.name
